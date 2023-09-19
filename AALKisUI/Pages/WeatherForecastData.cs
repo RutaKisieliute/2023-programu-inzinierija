3268a8c9d54523f2a01dd1a359122d54f089cbee
antanas-vasiliauskas <antanas.vasiliauskas.4@gmail.com>
2023-09-19 20:22:49 +0300

    replaced razor pages with mvc

--- AALKisUI/Pages/WeatherForecastData.cs
+++
@@ -1,8 +0,0 @@
-namespace AALKisUI.Pages;
-public class WeatherForecastData
-{
-    public DateTime Date { get; set; }
-    public int TemperatureC { get; set; }
-    public int TemperatureF { get; set; }
-    public string? Summary { get; set; }
-}
