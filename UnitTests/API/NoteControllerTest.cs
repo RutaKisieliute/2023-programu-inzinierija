bce34c2d5e23932f1a9fad253cae13c8fe231700
antanas-vasiliauskas <antanas.vasiliauskas.4@gmail.com>
2023-11-28 17:52:26 +0200

    Unit Test using Moq

---
+++ UnitTests/API/NoteControllerTest.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using Moq;
+using AALKisAPI.Controllers;
+using AALKisAPI.Services;
+using Microsoft.Extensions.Logging;
+using AALKisShared.Records;
+
+namespace UnitTests.API;
+public class NoteControllerTest
+{
+    private readonly NoteController _noteController;
+    private readonly Mock<INotesRepository> _noteRepoMock = new Mock<INotesRepository>();
+    private readonly ILogger<NoteController> _loggerMock = Mock.Of<ILogger<NoteController>>();
+
+    public NoteControllerTest()
+    {
+        _noteController = new NoteController(_loggerMock, _noteRepoMock.Object);
+
+    }
+
+    [Fact]
+    public async Task Get_ShouldReturnNote()
+    {
+        var noteId = 5;
+        var noteTitle = "Test Title";
+        var noteDto = new Note
+        {
+            Id = noteId,
+            Title = noteTitle,
+        };
+        _noteRepoMock.Setup(x => x.GetNote(noteId, false)).Returns(noteDto);
+
+        Note? note = await _noteController.Get(noteId);
+    
+        Assert.Equal(noteId, note.Value.Id);
+    }
+}
